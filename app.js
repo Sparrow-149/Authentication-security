32bcedb6b5577d4b09790a068adb248e63146aca
Sparrow-149 <amanjoshi149@gmail.com>
2023-07-04 14:55:16 +0530

    Added md5 hash encryption to our password

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -19,8 +20,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "";
-userSchema.plugin(encrypt,{secret: process.env.SECRET,encryptedFields:["password"]});
+// userSchema.plugin(encrypt,{secret: process.env.SECRET,encryptedFields:["password"]});
 
 
 
@@ -42,12 +42,10 @@ app.get('/register',function (req,res) {
 
 
 
-
-
 app.post("/register",function(req,res){
     const user = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     user.save().then(function(userregistered){
         if(userregistered)
@@ -59,7 +57,7 @@ app.post("/register",function(req,res){
 });
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email: username}).then(function(foundUser){
             if(foundUser){
                 if(foundUser.password === password){
@@ -74,9 +72,6 @@ app.post("/login",function(req,res){
                 res.render("login");
     });
 });
-
-
-
 
 
 app.listen(3000, function(){
